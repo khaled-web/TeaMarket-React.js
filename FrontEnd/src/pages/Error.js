ffc129da2e8a5aabb4cacca6f3d64209f6a87324
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-07-24 08:38:31 +0200

    update frontEnd

--- FrontEnd/src/pages/Error.js
+++ FrontEnd/src/pages/Error.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import img from '../assets/images/not-found.svg'
 import Wrapper from '../assets/wrappers/ErrorPage'
 
+
 const Error = () => {
   return (
     <Wrapper className='full-page'>
--- FrontEnd/src/pages/Home.js
+++ FrontEnd/src/pages/Home.js
@@ -1,19 +1,37 @@
 import React from 'react'
-import {Navbar, Header, Skill, About, Products, Services, Contact,Footer} from '../components'
+import {
+  Navbar,
+  Header,
+  Skill,
+  About,
+  Products,
+  Services,
+  Contact,
+  Footer
+} from '../components'
 
 const Home = () => {
-  return (
-    <main>
-      <Navbar/>
-      <Header/>
-      <div className="content-divider"></div>
-      <Skill/>
-      <About/>
-      <Products/>
-      <Services/>
-      <Contact/>
-      <Footer/>
-    </main>
+  return ( <
+    main >
+    <
+    Navbar / >
+    <
+    Header / >
+    <
+    div className = "content-divider" > < /div> <
+    Skill / >
+    <
+    About / >
+    <
+    Products / >
+    <
+    Services / >
+    <
+    Contact / >
+    <
+    Footer / >
+    <
+    /main>
   )
 }
 
