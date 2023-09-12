9fcdbfa186ef7c415d9742e3890a23ebdcdcf6e7
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-09-12 15:35:09 +0200

    updated - front end

--- FrontEnd/src/pages/Landing.js
+++ FrontEnd/src/pages/Landing.js
@@ -1,32 +1,17 @@
 import React from 'react'
-import Logo from '../components/Logo'
-import main from '../assets/images/SearchJobs.svg'
-import Wrapper from '../assets/wrappers/LandingPage'
 import {Link} from 'react-router-dom'
+import '../assets/css/header.css'
 
 
 const Landing = () => {
   return (
-    <Wrapper>
-     <nav>
-      <Logo/>
-     </nav>
-     <div className="container page">
-      {/* info */}
-      <div className='info'>
-       <h1>
-        job <span>tracking</span> app
-       </h1>
-       <p>
-       I'm baby vibecession wolf lumbersexual, next level stumptown JOMO dreamcatcher synth tote bag. Bodega boys messenger bag microdosing you probably haven't heard of them XOXO 8-bit mlkshk, tattooed selvage 90's cloud bread put a bird on it. Gatekeep listicle marfa enamel pin. 
-       </p>
-      <Link to = '/register' className = 'btn btn-hero' >
-        login/Register
-      </Link>
+    <header className='header-landing' id='hero'>
+      <div className="banner">
+        <h2>over one hundred flavors of</h2>
+        <h1>specially <br/> crafted tea</h1>
+        <Link to='/register' className='btn banner-btn'>Login / Register</Link>
       </div>
-      <img src={main} alt="job hunt" className='img main-img'/>
-     </div>
-    </Wrapper>
+    </header>
   )
 }
 
